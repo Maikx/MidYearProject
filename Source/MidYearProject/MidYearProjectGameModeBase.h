c13a8b216835585718a4d1caacd0672591fd178f
Maikx <mikeybee2000@gmail.com>
2022-03-03 18:18:09 +0100

    Re-uploaded empty project with C++ option

---
+++ Source/MidYearProject/MidYearProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MidYearProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MIDYEARPROJECT_API AMidYearProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
